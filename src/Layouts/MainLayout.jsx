65e7238c826ca7014f99774d72d67468adf80cff
shuvo sutradhor <shuvosd3837@gmail.com>
2024-02-23 20:33:19 +0600

    added navber

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 
 import { Outlet } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
+import Navber from '../Shared/Navber/Navber';
 
 const MainLayout = () => {
     return (
         <div>
-            <Outlet></Outlet>,
+            <Navber></Navber>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
